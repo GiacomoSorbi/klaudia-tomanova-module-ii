d78d3f07ed2209fec41c1b0f8df03f3fe003a683
Klaudia Tomanova <klaudiatomanova@Klaudias-MacBook-Air.local>
2020-07-13 09:14:19 +0200

    Updates

---
+++ precache-manifest.16fbebf17e3bce818945b72a732992e2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb1335824a077dec45abd22d16ff0a46",
+    "url": "/klaudia-tomanova-module-ii/index.html"
+  },
+  {
+    "revision": "dd11b2f3446273481b39",
+    "url": "/klaudia-tomanova-module-ii/static/css/main.4a1cfe4e.chunk.css"
+  },
+  {
+    "revision": "27a23928238b77ac9986",
+    "url": "/klaudia-tomanova-module-ii/static/js/2.e71209ce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/klaudia-tomanova-module-ii/static/js/2.e71209ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd11b2f3446273481b39",
+    "url": "/klaudia-tomanova-module-ii/static/js/main.208ece2d.chunk.js"
+  },
+  {
+    "revision": "27f27234eade559a2527",
+    "url": "/klaudia-tomanova-module-ii/static/js/runtime-main.be4cd13e.js"
+  },
+  {
+    "revision": "bd735a438c21284fddd4c939b61ee04b",
+    "url": "/klaudia-tomanova-module-ii/static/media/404.bd735a43.svg"
+  },
+  {
+    "revision": "54dc0ff88f50bd807bb3c73f0d39185d",
+    "url": "/klaudia-tomanova-module-ii/static/media/dress.54dc0ff8.jpg"
+  },
+  {
+    "revision": "c48ea14224ce3ae0882794b86f45822d",
+    "url": "/klaudia-tomanova-module-ii/static/media/hero.c48ea142.jpg"
+  },
+  {
+    "revision": "ad3fc6912003cf4019634aca88ea6752",
+    "url": "/klaudia-tomanova-module-ii/static/media/shirt.ad3fc691.jpg"
+  },
+  {
+    "revision": "d46ae72c28f50f000ce072575965a925",
+    "url": "/klaudia-tomanova-module-ii/static/media/shorts.d46ae72c.jpg"
+  }
+]);
